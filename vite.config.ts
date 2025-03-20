1707c0608398cebc5dff580f5c4bd104e9478f16
VIKASH J <112776733+Vikash888@users.noreply.github.com>
2025-03-20 10:16:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://vikash888.github.io/Contact-Info/',
         changeOrigin: true,
       },
     },
